fd78bc93ed5a205b3397960700a2a3105a33a793
Selim Ahmed <aronno1920@hotmail.com>
2025-02-20 16:20:04 +0600

    Update app.routes.ts

--- UI/NetForumUI/src/app/app.routes.ts
+++ UI/NetForumUI/src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { CategoryListComponent } from './features/category/category-list/category-list.component';
+import { AddCategoryComponent } from './features/category/add-category/add-category.component';
 
 export const routes: Routes = [
     {
         path:'admin/categories',
         component:CategoryListComponent
+    },
+    {
+        path:'admin/categories/add',
+        component:AddCategoryComponent
     }
 ];
 
