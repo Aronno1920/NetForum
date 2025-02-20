47100146d484c25002043b505b75706bf90844d1
Selim Ahmed <aronno1920@hotmail.com>
2025-02-20 14:54:35 +0600

    Update and added new in app.router
    @NgModule({
        imports: [
            HttpClientModule,
            RouterModule.forRoot(routes)
        ],
        exports: [RouterModule],
    })
    
    export class AppRoutingModule {}

--- UI/NetForumUI/src/app/app.routes.ts
+++ UI/NetForumUI/src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
-export const routes: Routes = [];
+import { CategoryListComponent } from './features/category/category-list/category-list.component';
+
+export const routes: Routes = [
+    {
+        path:'admin/categories',
+        component:CategoryListComponent
+    }
+];
+
+@NgModule({
+    imports: [
+        HttpClientModule,
+        RouterModule.forRoot(routes)
+    ],
+    exports: [RouterModule],
+})
+
+export class AppRoutingModule {}
